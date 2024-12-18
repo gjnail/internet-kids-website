4c091bbbe248edbcd7f220204a90f738304a1cb7
Greg Nail <gregnail@gmail.com>
2024-12-18 14:43:31 -0800

    feat: implement environment variables for Firebase config

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
@@ -13,12 +14,12 @@ class DefaultFirebaseOptions {
     );
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'internet-kids-plugins-site.firebaseapp.com',
-    projectId: 'internet-kids-plugins-site',
-    storageBucket: 'internet-kids-plugins-site.appspot.com',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    appId: 'YOUR_APP_ID',
+  static FirebaseOptions get web => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY'] ?? '',
+    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'] ?? '',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
+    appId: dotenv.env['FIREBASE_APP_ID'] ?? '',
   );
 } 
--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@ import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
